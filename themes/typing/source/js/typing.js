4791c98a00de4072866685a4d8912c6ce449cadf
curben <curben@users.noreply.gitlab.com>
2019-05-22 11:13:21 +0930

    refactor: remove jquery and fancybox
    todo https://github.com/dimsemenov/photoswipe

--- themes/typing/source/js/typing.js
+++ themes/typing/source/js/typing.js
@@ -1,30 +1,3 @@
-(($) => {
-  // Fancybox caption
-  $('.article-entry').each((i) => {
-    $(this).find('img').each(() => {
-      // Don't insert fancybox element to cloudinary's cld-responsive img class
-      if ($(this).hasClass('cld-responsive') || $(this).parent().hasClass('fancybox')) return
-
-      const alt = this.alt
-
-      if (alt) {
-        $(this).after('<span class="caption">' + alt + '</span>')
-      }
-
-      $(this).wrap('<a href="' + this.src + '" title="' + alt + '" class="fancybox"></a>')
-    })
-
-    $(this).find('.fancybox').each(() => {
-      $(this).attr('rel', 'article' + i)
-    })
-  })
-
-  if ($.fancybox) {
-    $('.fancybox').fancybox()
-  }
-
-})(jQuery)
-
 // Search button function
 const searchClick = document.getElementById('searchClick')
 searchClick.onclick = function() {
