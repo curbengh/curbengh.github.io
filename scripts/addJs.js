f90efdfe5f76ac1ff9056d5627f383982c278649
curben <2809763-curben@users.noreply.gitlab.com>
2019-08-26 08:14:21 +0930

    feat(addJs): add a helper to embed <script src="">

---
+++ scripts/addJs.js
@@ -0,0 +1,35 @@
+'use strict'
+/* global hexo */
+
+/*
+* Helper to add <script src=""> with support of custom attributes
+* https://github.com/hexojs/hexo/pull/3681
+*/
+
+function jsHelper (...args) {
+  let result = '\n';
+  let items = args;
+
+  if (!Array.isArray(args)) {
+    items = [args];
+  }
+
+  items.forEach(item => {
+    // Old syntax
+    if (typeof item === 'string' || item instanceof String) {
+      result += `<script src="${item}"></script>\n`;
+    } else {
+      // New syntax
+      let tmpResult = '<script';
+      for (const attribute in item) {
+        if (item[attribute] === true) tmpResult += ' ' + attribute;
+        else tmpResult += ` ${attribute}="${item[attribute]}"`;
+      }
+      tmpResult += '></script>\n';
+      result += tmpResult;
+    }
+  });
+  return result;
+}
+
+hexo.extend.helper.register('addJs', jsHelper)
