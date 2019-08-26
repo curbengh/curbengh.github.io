3187b18befb07fa8527bc5c4b683148185ef5273
curben <2809763-curben@users.noreply.gitlab.com>
2019-08-26 21:00:24 +0930

    docs(openGraph): og:image is now derived from <a>

--- scripts/openGraph.js
+++ scripts/openGraph.js
@@ -6,7 +6,7 @@
 * https://github.com/hexojs/hexo/blob/master/lib/plugins/helper/open_graph.js
 * for compatibility with cloudinary.js
 * the <meta name="og:image"> now use
-* data-src attribute of <img>, instead of src
+* <a> where href value links to an image
 */
 
 'use strict'
