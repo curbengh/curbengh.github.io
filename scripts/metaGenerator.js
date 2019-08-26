afb4741be66e334090f62d76db859468fd471ae4
curben <2809763-curben@users.noreply.gitlab.com>
2019-08-26 20:49:16 +0930

    feat(metaGenerator): drop metaGenerator in favor of Hexo's
    * https://github.com/hexojs/hexo/pull/3671

--- scripts/metaGenerator.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-/* global hexo */
-
-function metaGeneratorHelper () {
-  return '<meta name="generator" content="Hexo %s">'.replace('%s', this.env.version)
-}
-
-hexo.extend.helper.register('metaGenerator', metaGeneratorHelper)
