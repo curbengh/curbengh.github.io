ffa9c8c5beb4ba2c8b435e42c5d4cf91643d6f53
curben <2809763-curben@users.noreply.gitlab.com>
2019-10-19 04:12:15 +0100

    fix(copy_button): enable dotAll/'s' flag
    - <pre> could span multiple lines

--- themes/chameleon/scripts/copy-button.js
+++ themes/chameleon/scripts/copy-button.js
@@ -9,7 +9,7 @@ hexo.extend.filter.register('after_render:html', (data) => {
   const copyBtn = '<button class="copy-button">Copy</button>'
 
   // Regex is based on https://github.com/hexojs/hexo/pull/3697
-  return data.replace(/<pre>(?!<\/pre>).+?<\/pre>/ig, (str) => {
+  return data.replace(/<pre>(?!<\/pre>).+?<\/pre>/gs, (str) => {
     if (!str.includes(copyBtn)) return str.replace('</pre>', copyBtn + '</pre>')
     return str
   })
