feeaca491f9094cc2635396abb3569af780fbc8a
curben <2809763-curben@users.noreply.gitlab.com>
2019-12-20 02:44:20 +0000

    fix(js): load copy button script in post only

--- themes/chameleon/source/js/chameleon.js
+++ themes/chameleon/source/js/chameleon.js
@@ -6,34 +6,3 @@ document.getElementById('searchClick').addEventListener('click', () => {
 document.getElementById('searchClickMob').addEventListener('click', () => {
   searchFormMob.submit()
 }, false)
-
-/*
-* Copy button
-* Following functions only execute after
-* the 'document' is ready or
-* <script src> is executed.
-*/
-document.addEventListener('DOMContentLoaded', () => {
-
-  // Copy button
-  const copyCode = new ClipboardJS('.copy-button', {
-    target: (trigger) => {
-      return trigger.previousElementSibling
-    }
-  })
-
-  copyCode.on('success', (event) => {
-    event.clearSelection()
-    event.trigger.textContent = 'Copied'
-    window.setTimeout(() => {
-      event.trigger.textContent = 'Copy'
-    }, 2000)
-  })
-
-  copyCode.on('error', (event) => {
-    event.trigger.textContent = 'Press "Ctrl + C" to copy'
-    window.setTimeout(() => {
-      event.trigger.textContent = 'Copy'
-    }, 2000)
-  })
-}, false)
