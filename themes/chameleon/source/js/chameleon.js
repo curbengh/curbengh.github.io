a3739cf6de614fc255139ad40e389097872407eb
MDLeom <2809763-curben@users.noreply.gitlab.com>
2020-03-06 08:47:37 +0000

    feat: use duckduckgo onion address if site is .onion

--- themes/chameleon/source/js/chameleon.js
+++ themes/chameleon/source/js/chameleon.js
@@ -6,3 +6,12 @@ document.getElementById('searchClick').addEventListener('click', () => {
 document.getElementById('searchClickMob').addEventListener('click', () => {
   searchFormMob.submit()
 }, false)
+
+// Use duckduckgo's onion address when the site is accessed via .onion
+if (document.location.hostname.endsWith('.onion')) {
+  const searchForms = document.querySelectorAll('form#searchForm, form#searchFormMob')
+
+  searchForms.forEach((form) => {
+    form.setAttribute('action', 'https://3g2upl4pq6kufc4m.onion/')
+  })
+}
