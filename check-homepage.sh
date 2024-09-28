8042d4195a852ed8f7ab6c942b5f02d878b49ec4
Ming Di Leom <2809763-curben@users.noreply.gitlab.com>
2024-09-28 22:34:00 +0000

    ci: ensure microblog/ is not empty

--- check-homepage.sh
+++ check-homepage.sh
@@ -5,12 +5,12 @@
 HOMEPAGE="public/microblog/index.html"
 
 if [ ! -f "$HOMEPAGE" ]; then
-  echo "Error: homepage doesn't exist"
+  echo "Error: microblog doesn't exist"
   exit 1
 else
   FILE_SIZE=$(ls -s "$HOMEPAGE" | cut -d" " -f1)
   if [ "$FILE_SIZE" = 0 ]; then
-    echo "Error: homepage is empty"
+    echo "Error: microblog is empty"
     exit 1
   fi
 fi
