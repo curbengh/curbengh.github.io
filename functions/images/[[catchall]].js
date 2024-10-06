64a29d95a011ce589d17f567b18e93d0e8202408
Ming Di Leom <2809763-curben@users.noreply.gitlab.com>
2024-10-06 20:45:26 +0000

    fix(cf-pages): rewrite host
    worker host is restricted to mdleom.com

--- functions/images/[[catchall]].js
+++ functions/images/[[catchall]].js
@@ -1,12 +1,12 @@
-export async function onRequestGet(context) {
-  // const {pathname} = new URL(context.request.url)
-  // const imageURL = new URL(pathname, 'https://mdleom.com')
-  // const imageRequest = new Request(imageURL, {
-  //   headers: context.request.headers
-  // })
+export async function onRequestGet (context) {
+  const { pathname } = new URL(context.request.url)
+  const imageURL = new URL(pathname, 'https://mdleom.com')
+  const imageRequest = new Request(imageURL, {
+    headers: context.request.headers
+  })
   // return fetch(imageRequest)
 
   // CF_IMAGES binds to cf-images worker
   // configured in the pages dashboard
-  return context.env.CF_IMAGES.fetch(context.request)
+  return context.env.CF_IMAGES.fetch(imageRequest)
 }
