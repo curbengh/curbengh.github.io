3d45602df9dbab071b3a8eac1b2481c99536c24c
Ming Di Leom <2809763-curben@users.noreply.gitlab.com>
2024-10-06 08:37:19 +0000

    build: deploy microblog to mirrors

--- build.sh
+++ build.sh
@@ -11,9 +11,23 @@ export PATH="$PATH:./node_modules/.bin"
 
 hexo generate
 
-# deploy site assets
 if [ "$NODE_ENV" = "production" ] && [ -d "public/" ]; then
+  # deploy site assets
   git clone --depth 1 --branch site https://gitlab.com/curben/blog.git site
   cp -r site/* "public/"
   rm -f "public/README.md"
+
+  # deploy microblog
+  git clone --depth 1 --branch microblog https://gitlab.com/curben/blog.git microblog
+  cd "microblog/"
+  export PATH="$PATH:../node_modules/.bin"
+  hexo generate
+
+  cat "rsync-include.txt" | while read include; do
+    find "public/" -path "public/$include" -type f | while read file; do
+      destdir="../public/$(dirname $file | sed -r 's|^public/?||')"
+      mkdir -p "$destdir"
+      cp "$file" "$destdir"
+    done
+  done
 fi
